0702606e4055eec522f5037b4442bfdd70ebe357
buttonninja <buttonninja@gmail.com>
2026-08-24 20:59:18 +0000

    commit

---
+++ src/main/java/com/neueda/leap/Main.java
@@ -0,0 +1,7 @@
+package com.neueda.leap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world from the team's Sprint 2 project skeleton");
+    }
+}
